23f87b60ca13c544340d8323667f086220243ae3
Ashisoma <okalionesmus@gmail.com>
2020-09-03 20:32:48 +0300

    responsive odering page

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
-function Order(size, crust, toppings){
-    this.size = size;
-    this.crust = crust;
-    this.toppings = top;
+function getSize() {
+    var size = document.getElementById("sizes").value;
+    return parseInt(size);
 }
+function getFlavour() {
+    var flavour = document.getElementById("crusts").value;
+    return parseInt(flavour);
+}
+function getTopping() {
+    var topping = document.getElementById("toppings").value;
+    return parseInt(topping);
+}
+function getNumber() {
+    var number = document.getElementById("number").value;
+    return parseInt(number);
+}
+function calculateTotal() {
+    var results = (getSize() + getFlavour() + getTopping()) * getNumber();
+    alert("You have ordered " + getNumber("") + " Pizza " + " which amounts to Kshs. " + results + " Thank you ");
+    prompt("Enter your location");
+    alert("Your order has been received and will be delivered in a short while. Delivery fee is kshs 150.");
+}
+function calculate() {
+    var results = (getSize() + getFlavour() + getTopping()) * getNumber();
 
-$(document).ready(function(){
-    $("form#make-order").click(function(event){
-        event.preventDefault();
+    alert("You have ordered " + getNumber("") + " Pizza " + " which amounts to Kshs. " + results + " Your order is ready for pickup at our station along Moi Avenue . Contact us for more information. Thank you.");
 
-        var orderedSize = $("#sizes").val("")
-        var orderedCrust = $("#crusts").val("")
-        var orderedToppings = $("#toppings").val("")
-        var newOrder = new Order(orderedToppings, " Pizza", orderedCrust, orderedSize)
-
-
-        $("option#sizes").val("");
-        $("option#crusts").val("");
-        $("option#toppings").val("");
-
-       $("#order").last().click(function(){
-        $("#new-size").text(newOrder.orderedSize)
-        $("#new-crusts").text(newOrder.orderedCrust)
-        $("#new-topping").text(newOrder.orderedToppings)
-       })
-        
-
-    })
-})
+}
